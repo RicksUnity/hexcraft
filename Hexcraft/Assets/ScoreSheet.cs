766e6ceca25d31ec623f5cbcf237f3b729ca28cb
James <james.at.todd@gmail.com>
2018-02-14 17:05:39 +0000

    Added player movement
    Delete the text and plane as needed

---
+++ Hexcraft/Assets/ScoreSheet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSheet : MonoBehaviour
+{
+    public Text scoreText;
+    private int counteru;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreText.text = "bloooooooooooopie";
+        counteru = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "blooooootyujhgooooopie";
+    }
+}
