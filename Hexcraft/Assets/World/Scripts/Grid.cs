96a742371b1db54cc900fedb96f0e45538cbea4c
Efrain18*37 <efrain.j.mendez@gmail.com>
2018-02-16 13:59:06 +0000

    Made map in assets
    Made a flat grid map script and a scene with a 100x100 blocks

---
+++ Hexcraft/Assets/World/Scripts/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid: MonoBehaviour {
+
+	public Transform pointPrefab;
+
+	public int box_x2 = 20;
+
+
+
+	void Awake () {
+
+		Vector3 scale = Vector3.one;
+		Vector3 position;
+		position.y = 0f;
+		float tres = Mathf.Sqrt (3);
+
+
+		for (int j = -box_x2; j < box_x2; j++) {
+			for (int i = -box_x2; i < box_x2; i++) {
+				Transform point = Instantiate(pointPrefab);
+				position.x = (j+i*2f);
+				position.z = (j*(1f*tres));
+				point.localPosition = position;
+				point.localScale = scale;
+				point.SetParent(transform, false);
+
+			}
+		}
+
+
+
+	}
+}
