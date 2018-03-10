7d7c53d417e0e6aee4f6c17b4c8324d02ff5d651
alzom <alex.morozovsky@gmail.com>
2018-03-10 15:36:01 +0000

    Gui Inventory
    Basic GUI inventory implemented.

--- Hexcraft/Assets/Scripts/Inventory.cs
+++ Hexcraft/Assets/Scripts/Inventory.cs
@@ -3,24 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Inventory : MonoBehaviour {
+	public int slotsX, slotsY;
 	public List<Item> inventory = new List<Item>();
+	public List<Item> slots = new List<Item>(); 
+	private bool showInventory;   
 	private ItemDatabase database; 
 	void Start() {
+		for (int i = 0; i < (slotsX*slotsY); i++){
+			slots.Add(new Item());
+		}
 		database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>(); 
-		
-		print(database.items[0].itemName);
-		print(database.items[1].itemName);
-		print(inventory.Count);
+
 		inventory.Add (database.items[0]);
 		inventory.Add (database.items[1]);
-		print(inventory[0].itemName);
-		print(inventory[1].itemName);
 
 	}
+	void Update(){
+		if(Input.GetButtonDown("Inventory")){
+			showInventory =  !showInventory;
+		}
+	}
 	void OnGUI(){
+		if(showInventory){
+			DrawInventory();
+		}
 		for (int i = 0; i<inventory.Count; i++){
 			GUI.Label(new Rect(10,i*20,200,50),inventory[i].itemName);
 		}
+	}
+	void DrawInventory(){
+		for (int x= 0; x < slotsX; x++){
+			for (int y = 0; y < slotsY; y ++){
+				GUI.Box(new Rect(x * 20, y * 20, 20, 20), y.ToString());
+			}
+
+		}
+		
 	}
 
 
--- Hexcraft/Assets/Scripts/Item.cs
+++ Hexcraft/Assets/Scripts/Item.cs
@@ -15,7 +15,21 @@ public class Item {
         Weapon,
         Block
     }
+    public Item(string name,int id, string desc, int power, int speed, ItemType type){
+        itemName = name; 
+        itemID = id; 
+        itemDesc = desc; 
+        itemIcon = Resources.Load<Texture2D>("Item Icons/"+ name); 
+        itemPower = power;
+        itemSpeed = speed; 
+        itemType = type; 
 
+
+    }
+    public Item(){
+        
+    }
+    
 
 
 }
