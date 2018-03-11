bf88bcbc4c726884d4f05161645f4620a3e889f8
alzom <alex.morozovsky@gmail.com>
2018-03-11 15:08:41 +0000

    Health script
    Added the health script

--- Hexcraft/Assets/Scripts/Health.cs
+++ Hexcraft/Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health.cs : MonoBehaviour {
+public class Health : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
