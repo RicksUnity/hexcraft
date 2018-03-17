acc732b40660ce2fe9d7c76e6212757438b6e8b0
James <james.at.todd@gmail.com>
2018-03-17 19:33:29 +0000

    Redstone Implementation 1
    The first stages of implementing redstone into the game. Specifically, added place holders for both redstone and redstone torches, added basic redstone functionality where redstone passes on it's state to nearby redstone blocks

---
+++ Hexcraft/Assets/Scripts/RedstoneBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedstoneBehaviour : MonoBehaviour {
+    public int strength = 0;
+    public bool torch = false;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (torch == false)
+        {
+            Collider[] nearby = Physics.OverlapSphere(transform.position, 2.1f);
+            strength = 0;
+            for (int i = 0; i < nearby.Length; i++)
+            {
+                if (nearby[i].name == "redstone(Clone)" && nearby[i].GetComponent<RedstoneBehaviour>().strength > strength + 1)
+                {
+                    strength = nearby[i].GetComponent<RedstoneBehaviour>().strength - 1;
+                }
+
+            }
+            
+        }
+        else
+        {
+            if (GetComponent<DropMechanics>().isDropped != true)
+            {
+                if(GetComponent<DropMechanics>().attatchedTo.GetComponent<DropMechanics>().isPowered == false)
+                {
+                    strength = 12;
+                }
+                else
+                {
+                    strength = 0;
+                }
+            }
+        }
+    }
+}
