7a8236fc4e30c4d6d183d07e7a79da6ab93e28d3
alzom <alex.morozovsky@gmail.com>
2018-04-12 22:04:54 +0100

    Merge SaveLoad2 into Develop

--- Hexcraft/Assets/Scripts/Inventory.cs
+++ Hexcraft/Assets/Scripts/Inventory.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 public class Inventory : MonoBehaviour {
 	public int slotsX, slotsY;
 	public GUISkin skin;
@@ -16,17 +16,34 @@ public class Inventory : MonoBehaviour {
 	private bool draggingItem; 
 	private Item draggedItem; 
 	private int prevIndex; 
-	
+	public List<Item> Craftslots = new List<Item> ();
+	public List<Item> Craftinventory = new List<Item>();
+	private CraftingRecipe Recipe;
+	public Rect finishBox = new Rect();
 	void Start() {
 		for (int i = 0; i < (slotsX*slotsY); i++)
 		{
 			slots.Add(new Item());
+<<<<<<< HEAD
 			inventory.Add(new Item());
 			favSlots.Add(new Item());
 		}
 		database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>(); 
 		addItem(0);
 		addItem(1);
+=======
+			inventory.Add (new Item());
+			Craftslots.Add (new Item ());
+			Craftinventory.Add (new Item());
+		}
+		database = GameObject.FindGameObjectWithTag("Item Database").GetComponent<ItemDatabase>();
+		Recipe = GameObject.FindGameObjectWithTag("Crafting Recipe").GetComponent<CraftingRecipe>();
+
+		addItem(1);
+		addItem(1);
+		//addCraftingItem (1);
+		//addCraftingItem (1);
+>>>>>>> SaveLoad2
 	}
 	void Update()
 	{
@@ -38,8 +55,14 @@ public class Inventory : MonoBehaviour {
 			
 		}
 	}
+<<<<<<< HEAD
 	void OnGUI()
 	{
+=======
+
+	void OnGUI(){
+		//Debug.Log (inventory[0].itemID);
+>>>>>>> SaveLoad2
 		tooltip = "";
 		GUI.skin = skin;  
 		if(showInventory)
@@ -67,50 +90,108 @@ public class Inventory : MonoBehaviour {
 	{
 		Event e = Event.current; 
 		int i = 0; 
+<<<<<<< HEAD
 		GUI.BeginGroup (new Rect (Screen.width / 2 - 250, Screen.height / 3 - 50, 600, 250));
 		GUI.Box(new Rect(0,0,600,250), "\n<color=#0>Glorious Inventory!</color>", skin.GetStyle("Background"));
 		for (int y = 1; y < slotsY+1; y ++)
 		{
 			for (int x = 1; x < slotsX+1; x++)
 			{
+=======
+		int x2 = slotsX * 50  ;
+		int y2 = 1; 
+
+		GUI.BeginGroup (new Rect (Screen.width / 2 - 400, Screen.height / 3 - 50, 900, 250));
+		GUI.Box(new Rect(0,0,900,250), "\n<color=#0>Glorious Inventory!</color>", skin.GetStyle("Background"));
+		for (int y = 1; y < slotsY+1; y ++){
+			for (int x = 1; x < slotsX+1; x++){
+				if (x2 > (slotsX * 50 + 100) ) {
+					y2 ++ ; 
+					x2 = slotsX * 50; 
+				}
+				x2 += 50;	
+>>>>>>> SaveLoad2
 				Rect slotRect = new Rect(x * 50, y * 50, 50, 50); 
+				Rect craftBox = new Rect(x2 + 35, y2 * 50, 50 ,50);
+
 				GUI.Box(new Rect(slotRect), "", skin.GetStyle("Slot"));
+				if (y2 < 4) {
+					GUI.Box (new Rect (craftBox), "", skin.GetStyle ("Slot"));
+					Craftslots [i] = Craftinventory [i];
+				}
+
 				slots[i] = inventory[i];
+<<<<<<< HEAD
 				if (slots[i].itemName != null)
 				{
 					GUI.DrawTexture(slotRect, slots[i].itemIcon);
 					if (slotRect.Contains(e.mousePosition))
 					{
+=======
+
+				if (slots[i].itemName != null||Craftslots[i].itemName!=null){
+					GUI.DrawTexture (craftBox, Craftslots[i].itemIcon);
+					GUI.DrawTexture(slotRect, slots[i].itemIcon);
+					if (slotRect.Contains(e.mousePosition)||craftBox.Contains(e.mousePosition)){
+>>>>>>> SaveLoad2
 						tooltip = CreateToolTip(slots[i]);
 						showToolTip = true; 
 						if (e.button == 0 && e.type == EventType.MouseDrag && !draggingItem)
 						{
 							draggingItem = true;
 							prevIndex = i; 
-							draggedItem  = slots[i]; 
-							inventory[i] = new Item();
-
+							if (craftBox.Contains (e.mousePosition)) {
+								draggedItem = Craftslots [i];
+								Craftinventory [i] = new Item ();
+							}
+							
+							else {
+								draggedItem = slots [i]; 
+								inventory [i] = new Item ();
+							}				 
 						}
 						
+<<<<<<< HEAD
 						if (e.type == EventType.MouseUp && draggingItem)
 						{
 							inventory[prevIndex] = inventory[i]; 
 							inventory[i] = draggedItem; 
+=======
+						if (e.type == EventType.MouseUp && draggingItem){							 
+							if (slotRect.Contains (e.mousePosition)) {
+								inventory [prevIndex] = inventory [i];
+								inventory [i] = draggedItem;
+							} else {
+								Craftinventory [prevIndex] = Craftinventory [i];
+								Craftinventory [i] = draggedItem;
+							}
+>>>>>>> SaveLoad2
 							draggingItem = false; 
 							draggedItem = null; 
-
-
 						}
 
 					}
 
 				} else {
+<<<<<<< HEAD
 					if(slotRect.Contains(e.mousePosition))
 					{
 						if (e.type == EventType.MouseUp && draggingItem)
 						{
 							inventory[prevIndex] = inventory[i]; 
 							inventory[i] = draggedItem; 
+=======
+					if(slotRect.Contains(e.mousePosition)||craftBox.Contains(e.mousePosition)){
+						if (e.type == EventType.MouseUp && draggingItem){
+							if (slotRect.Contains (e.mousePosition)) {
+								inventory [prevIndex] = inventory [i]; 
+								inventory [i] = draggedItem; 
+							}
+							else {
+								Craftinventory [prevIndex] = Craftinventory [i];
+								Craftinventory [i] = draggedItem;
+							}
+>>>>>>> SaveLoad2
 							draggingItem = false; 
 							draggedItem = null; 
 							
@@ -124,9 +205,19 @@ public class Inventory : MonoBehaviour {
 				}
 
 				i++; 
-			
 		}
+			//Debug.Log ("gg"+GetCraftID(Craftinventory));
+			//GUI.DrawTexture (finishBox, GetCraftID(Craftinventory));
+
 	}
+		finishBox = new Rect (x2+75,50,50,50);
+		GUI.Box (new Rect (finishBox), "", skin.GetStyle ("Slot"));
+		for (int k = 0; k < database.items.Count; k++){
+			if (database.items[k].itemID == GetCraftID(Craftinventory)){
+				GUI.DrawTexture (finishBox, database.items[k].itemIcon );
+				//Craftinventory[i] = database.items[j];
+			}
+		}
 	GUI.EndGroup ();
 	}
 	void DrawFavourites()
@@ -167,8 +258,29 @@ public class Inventory : MonoBehaviour {
 				break;
 			}
 		}
+
 	}
+	void addCraftingItem (int id){
+		for (int i = 0; i< Craftinventory.Count; i++){
+			if (Craftinventory[i].itemName == null){
+				for (int j = 0; j < database.items.Count; j++){
+					if (database.items[j].itemID == id){
+						Craftinventory[i] = database.items[j];
+					}
+				}
+				break;
+			}
+		}
+	}
+<<<<<<< HEAD
 	public void RemoveItem(int id){
+=======
+
+
+
+
+	void RemoveItem(int id){
+>>>>>>> SaveLoad2
 		for (int i = 0; i <inventory.Count; i++){
 			if (inventory[i].itemID == id){
 				inventory[i]= new Item();
@@ -188,7 +300,29 @@ public class Inventory : MonoBehaviour {
 		return result; 
 	}
 
-	
+	int GetCraftID(List<Item> Craftinventory){
+		foreach (KeyValuePair<int,int[]> items in Recipe.craftingRecipe) {
+			List<int> tempRecipe = new List<int> ();
+			List<int> tempCraftinventory = new List<int> ();
+			foreach (int values in items.Value) 				
+				tempRecipe.Add (values);
+			
+			for (int v = 0; v < Craftinventory.Count; v++) {
+				tempCraftinventory.Add (Craftinventory [v].itemID);
+				//Debug.Log (Craftinventory [v].itemID);
+			}
+			tempCraftinventory.RemoveRange (9, tempCraftinventory.Count-9);
+			//Debug.Log(tempCraftinventory.Count+"//////"+tempRecipe.Count);
+			//foreach (int ttt in tempCraftinventory)
+				//Debug.Log (ttt);
+
+			if (tempCraftinventory.SequenceEqual(tempRecipe)) 
+				return items.Key;
+			else
+				return 0;
+		}
+		return 0;
+	}
 }
 
 
--- Hexcraft/Assets/Scripts/MainMenu.cs
+++ Hexcraft/Assets/Scripts/MainMenu.cs
@@ -2,35 +2,72 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using System.IO;
+using UnityEditor;
+
 public class MainMenu : MonoBehaviour {
 
 	public enum Menu {
 		MainMenu,
 		NewGame,
-		LoadGame
+		LoadGame,
+		None
 	}
-
+	void Start()
+	{
+		GameObject loadBlock = GameObject.Instantiate (Resources.Load("grass ground") as GameObject);
+		loadBlock.transform.parent = GameObject.FindGameObjectWithTag ("LoadGame").transform;
+		loadBlock.transform.SetParent(GameObject.FindGameObjectWithTag ("LoadGame").transform,false);
+		loadBlock.transform.position = new Vector3(20.0f, 15.79f, 20.0f);
+		//transform.position = 
+		Vector3 myPos = GameObject.FindGameObjectWithTag ("LoadGame").transform.position;
+	}
+	/*void start(string name){
+		saveload.Load (name);
+	} */
+	//
 	int i=0;
 	public Menu currentMenu;
 	public string worldName;
 	public string characterName;
 	bool IsEscape;
-
+	public bool Isload;
+	SaveLoad2 saveload = new SaveLoad2 ();
 
 	public static DirectoryInfo dir = new DirectoryInfo("Assets/Store/");
-	public static FileInfo[] info = dir.GetFiles ("*.txt");
+	public static FileInfo[] info = dir.GetFiles ("*.dat");
 	//Debug.Log (i);
-
-
-	void OnGUI () {
-		if (Input.GetKeyDown ("escape")) {
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log ("update");
+			//GUI.enabled = true;
 			if (!IsEscape)
-		IsEscape = true;
+				IsEscape = true;
 			else
 				IsEscape = false;
-			Debug.Log (IsEscape);
 		}
+
+	}
+
+	void OnGUI () {
+		GUILayout.BeginArea(new Rect(0,0,Screen.width, Screen.height));
+		GUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace();
+		GUILayout.BeginVertical();
+		GUILayout.FlexibleSpace();
+		//Debug.Log ("f");
+
+		/*if (Input.GetKeyDown ("escape")) {
+				Debug.Log ("Escape0");
+			if (!IsEscape)
+				IsEscape = true;
+			else
+				IsEscape = false;
+				Debug.Log (IsEscape);
+			}*/
+		//if (currentMenu == Menu.InGame) {		
 		if (IsEscape) {
+			//GUI.enabled = true;
+			//Debug.Log ("Escape1");
 			GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
 			GUILayout.BeginHorizontal ();
 			GUILayout.FlexibleSpace ();
@@ -39,19 +76,17 @@ public class MainMenu : MonoBehaviour {
 
 			if (GUILayout.Button ("Back to the game")) {
 				IsEscape = false;
-			} 
-			else if (GUILayout.Button ("Save the game")) {
-
-				SaveLoad.Save (worldName);
-				Debug.Log ('b');
-			} 
-			else if (GUILayout.Button ("Save the game and quit")) {
+			} else if (GUILayout.Button ("Save the game")) {
+				if (EditorUtility.DisplayDialog ("Save the game","Are you sure you want to save the game?","Yes","No"))
+					saveload.Save (worldName);
+					
+				//Debug.Log (EditorUtility.DisplayDialog ("Save the game","Are you sure you want to save the game?","Yes","No"));
+			} else if (GUILayout.Button ("Save the game and quit")) {
 
 				//Debug.Log ('c');
-			} 
-			else if (GUILayout.Button ("Quit without saving")) {
+			} else if (GUILayout.Button ("Quit without saving")) {
 				//Debug.Log ('d');
-				Application.Quit();
+				Application.Quit ();
 			}
 
 			GUILayout.FlexibleSpace ();
@@ -60,17 +95,13 @@ public class MainMenu : MonoBehaviour {
 			GUILayout.EndHorizontal ();
 			GUILayout.EndArea ();
 		}
+		//}
 		//Cursor.visible = true;
-		GUILayout.BeginArea(new Rect(0,0,Screen.width, Screen.height));
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
-		GUILayout.BeginVertical();
-		GUILayout.FlexibleSpace();
 
-		if(currentMenu == Menu.MainMenu) {
+		else if(currentMenu == Menu.MainMenu) {
 
 			if(GUILayout.Button("New Game")) {
-				Game.current = new Game();
+				//Game.current = new Game();
 				currentMenu = Menu.NewGame;
 			}
 			if(GUILayout.Button("Load Game")) {
@@ -86,20 +117,22 @@ public class MainMenu : MonoBehaviour {
 		else if (currentMenu == Menu.NewGame) {
 
 			GUILayout.Box("Name Your Characters");
-			//Debug.Log ("LKJDLKJDFLKSDFJLKSDFJ");
-			Game.current.player.name = GUILayout.TextField(Game.current.player.name, 20);
+			characterName = GUILayout.TextField(characterName, 20);
 			GUILayout.Space(10);
 
 			GUILayout.Box("Name your world");
-			Game.current.world.name = GUILayout.TextField(Game.current.world.name, 20);
-			worldName = Game.current.world.name;
+			worldName = GUILayout.TextField(worldName, 20);
+			//worldName = Game.current.world.name;
 			GUILayout.Space(10);
 
 			if(GUILayout.Button("Create the world!")) {
-				SaveLoad.Save(worldName);
-				this.enabled = false;
-				//SceneManager.LoadScene("map",LoadSceneMode.Single);
-				//SceneManager.LoadScene("Scene/New Game",LoadSceneMode.Single);
+				if (characterName.Length == 0 || worldName.Length == 0)
+					EditorUtility.DisplayDialog ("Empty name!", "Character's or world's name can't be empty", "OK");
+				else {
+					currentMenu = Menu.None;
+					saveload.Save (worldName);
+				}
+
 			}
 
 			GUILayout.Space(10);
@@ -109,7 +142,7 @@ public class MainMenu : MonoBehaviour {
 		}
 
 		else if (currentMenu == Menu.LoadGame) {
-			
+
 
 			//SaveLoad.Load();
 			GUILayout.Box("Select Saved File");
@@ -118,13 +151,16 @@ public class MainMenu : MonoBehaviour {
 
 
 			//if(i<info.Length){
-				foreach (FileInfo f in info){
-					//Debug.Log (i);
-					GUILayout.Space(10);
-					GUILayout.Button(Path.GetFileNameWithoutExtension(f.ToString()));
-
-					i++;
+			foreach (FileInfo f in info){
+				//Debug.Log (i);
+				GUILayout.Space(10);
+				if (GUILayout.Button (Path.GetFileNameWithoutExtension (f.ToString ()))) {
+					saveload.Load (f.ToString ());
+					currentMenu = Menu.None;
 				}
+				//currentMenu = Menu.None;
+				i++;
+			}
 			//}
 			//Debug.Log(i);
 
