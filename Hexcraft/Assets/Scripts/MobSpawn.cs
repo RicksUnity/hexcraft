70e42ad773abb53ac8552a41b6c4aeefd540be06
James <james.at.todd@gmail.com>
2018-04-28 21:23:27 +0100

    Tweaks and comments
    Added tweaks and comments to mobs and Player

--- Hexcraft/Assets/Scripts/MobSpawn.cs
+++ Hexcraft/Assets/Scripts/MobSpawn.cs
@@ -9,29 +9,31 @@ public class MobSpawn : MonoBehaviour {
     private float lightDistance = 14f;
     private GameObject Sun = GameObject.Find("Sun");
 
+    // --- CHeck to see if a mob should spawn every set amount of time defined by spawnCountdown
     void Start () {
         InvokeRepeating("CheckSpawn", 1, spawnCountdown);
 	}
-
+    // --- CHecks whether a MOB cna spwn then spawns it ---
     void CheckSpawn()
     {
-        print("Cheese Muffin");
+        // Only spawn if it is night
         if (Sun.GetComponent<DayNight>().time >= 0.25 && Sun.GetComponent<DayNight>().time <= 0.75)
         {
             Collider[] Enemies = Physics.OverlapSphere(transform.position, spawnRadius);
             int enemyCount = 0;
+            //Checks to see if there are at least five enemies nearby
             for (int i = 0; i < Enemies.Length; i++)
             {
                 if (Enemies[i].gameObject.tag == "Enemy")
                 {
                     enemyCount += 1;
-                    print(enemyCount);
                     if (enemyCount >= 5)
                     {
                         break;
                     }
                 }
             }
+            //If there are less than five enemies nearby then try to find a place to spawn and then spawn a MOB
             if (enemyCount < 5)
             {
                 for (int i = 0; i < Enemies.Length; i++)
@@ -50,7 +52,7 @@ public class MobSpawn : MonoBehaviour {
             }
         }
     }
-
+    //--- Check to see if there is any block directly above the current one to see if a MOB can spawn on it ---
     bool AboveCheck(GameObject spawnPos)
     {
         Collider[] Above = Physics.OverlapSphere(spawnPos.transform.position, 1.5f);
@@ -63,7 +65,7 @@ public class MobSpawn : MonoBehaviour {
         }
         return true;
     }
-
+    //--- Check whether there are any torches nearby as mobs cannot spawn if there are nearby torches ---
     bool TorchCheck(GameObject spawnPos)
     {
         Collider[] lightCheck = Physics.OverlapSphere(spawnPos.transform.position, lightDistance);
